c1fec8fddb8d9ae1919a41b871c42a1e1c7dadc2
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2023-11-20 19:07:54 +0530

    added key detection from keyboard

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 word = ""
+guess = 1
+character = 1
 
-guess1 = ""
-guess2 = ""
-guess3 = ""
-guess4 = ""
-guess5 = ""
-currentGuess = 1
-guessChar = 1
+const keys = {
+    "KeyA": () =>{console.log("a")},
+    "KeyB": () =>{console.log("b")},
+    "KeyC": () =>{console.log("c")},
+    "KeyD": () =>{console.log("d")},
+    "KeyE": () =>{console.log("e")},
+    "KeyF": () =>{console.log("f")},
+    "KeyG": () =>{console.log("g")},
+    "KeyH": () =>{console.log("h")},
+    "KeyI": () =>{console.log("i")},
+    "KeyJ": () =>{console.log("j")},
+    "KeyK": () =>{console.log("k")},
+    "KeyL": () =>{console.log("l")},
+    "KeyM": () =>{console.log("m")},
+    "KeyN": () =>{console.log("n")},
+    "KeyO": () =>{console.log("o")},
+    "KeyP": () =>{console.log("p")},
+    "KeyQ": () =>{console.log("q")},
+    "KeyR": () =>{console.log("r")},
+    "KeyS": () =>{console.log("s")},
+    "KeyT": () =>{console.log("t")},
+    "KeyU": () =>{console.log("u")},
+    "KeyV": () =>{console.log("v")},
+    "KeyW": () =>{console.log("w")},
+    "KeyX": () =>{console.log("x")},
+    "KeyY": () =>{console.log("y")},
+    "KeyZ": () =>{console.log("z")},
+
+}
+
+
+
+document.addEventListener('keydown', logKey);
+function logKey(e) {
+    keys[e.code]()
+}
+
+
+
 
